cc952fd155648a01b3212167e6346fb136d97dd5
Gichohi Karuga <fgkaruga@gmail.com>
2022-07-06 09:41:44 +0300

    project setup

---
+++ src/test/java/com/degitech/rec/risk/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.degitech.rec.risk.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
